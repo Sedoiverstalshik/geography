392f03c7a3e8596bd9edf00d6a39bc83658dde5d
Sedoiverstalshik <104726458+Sedoiverstalshik@users.noreply.github.com>
2022-12-12 21:39:17 +0300

    Add files via upload
    Сверстана главная страница с адаптивом и ховер, бургер меню.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+const burgerBtn = document.querySelector('.burger__menu');
+const burgerMenu = document.querySelector('.header__menu');
+const menuLink = document.querySelectorAll('.header__menu-list li a');
+
+burgerBtn.addEventListener('click', () => {
+  burgerMenu.classList.toggle('active');
+  burgerBtn.classList.toggle('active');
+})
+
+menuLink.forEach(link => {
+  link.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+  })
+})
+
+
+
 const intro__slider = new Swiper('.intro__swiper', {
   loop: true,
   slidesPerView: 1,
@@ -22,9 +40,27 @@ const reviews__slider = new Swiper('.reviews__swiper', {
 
 const tours__slider = new Swiper('.tours__swiper', {
   loop: true,
-  slidesPerView: 4,
   centeredSlides: true,
   spaceBetween: 70,
   grabCursor: true,
+  breakpoints: {
+    400: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+    },
+    551: {
+      spaceBetween: 20,
+    },
+    751: {
+      spaceBetween: 40,
+    },
+    1024: {
+      spaceBetween: 50,
+    },
+    1200: {
+      slidesPerView: "auto",
+      spaceBetween: 70,
+    },
+  },
 });
 
